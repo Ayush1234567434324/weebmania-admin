7c099168156e53f6665e2835b0df5d4736e69c11
Ayush1234567434324 <ayushbhandari3031@gmail.com>
2024-12-17 22:30:50 +0530

    admin-react-ui

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const path = require("path"); // Import the path module
+
 const app = express();
 
-// Basic route to check the backend is running
-app.get("/", (req, res) => {
-    res.send("Express on Vercel is working!");
+// Serve static files from React's build folder
+app.use(express.static(path.join(__dirname, "../admin", "build")));
+
+// API Route example
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from Express API!" });
 });
+
+// For any other route, serve the React app's index.html
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "build", "index.html"));
+});
+
+// Start the server
+
 
 module.exports = app; // Export the app
